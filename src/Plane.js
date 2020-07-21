041337a2ef1a444f161a979cc8143f2897ff8b43
mattbesedick <mattbezy@gmail.com>
2020-07-21 12:55:51 -0500

    updated plane for physics :

--- src/Plane.js
+++ src/Plane.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { usePlane } from 'use-cannon'
 
 
-const Plane = () => {
+const Plane = (props) => {
+    const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
     return (
-        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <meshPhysicalMaterial attach="material" color="gray" />
+        <mesh ref={ref} receiveShadow>
+            <planeBufferGeometry attach="geometry" args={[1009, 1000]} />
+            <shadowMaterial attach="material" color="red" />
         </mesh>
+        // <mesh rotation={[-Math.PI / 200, 0, 0]} position={[0, -3, 0]} receiveShadow >
+        //     <planeBufferGeometry attach="geometry" args={[100, 100]} />
+        //     <shadowMaterial attach="material" opacity={.2} />
+        // </mesh>
     )
 }
 
