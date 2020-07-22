de265cdefca9c993ed32cbe95148378af376119f
mattbesedick <mattbezy@gmail.com>
2020-07-21 22:26:27 -0500

    changed name to text for possibility of taking in props

---
+++ src/Text.js
@@ -0,0 +1,34 @@
+import React, { useMemo, useRef } from 'react'
+import { useLoader, useFrame } from 'react-three-fiber'
+import * as THREE from 'three'
+import { useSpring, a } from 'react-spring/three'
+
+const Michael = ({ children, position, props }) => {
+    const font = useLoader(THREE.FontLoader, '/bold.blob')
+    const config = useMemo(() => ({
+        font, size: 60, height: 30
+    }), [font])
+    const mesh = useRef()
+    // const props = useSpring({
+    //     position: [-200, 0, 100]
+    // })
+    // const [mesh] = useBox(() => ({ mass: 1 }))
+    useFrame(() => {
+        if (mesh.current.position.y > 4) {
+            mesh.current.position.y -= Math.random() * 10
+        }
+    })
+
+
+
+    return (
+        <group scale={[.01, .01, .01]} >
+            <mesh position={position} receiveShadow castShadow ref={mesh}>
+                <textBufferGeometry attach="geometry" args={[children, config]} />
+                <meshPhysicalMaterial attach="material" color="white" />
+            </mesh>
+        </group>
+    )
+}
+
+export default Michael
