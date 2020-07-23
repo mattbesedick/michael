4131e33c7f53ad5b92dc946a1ae66c547c199cef
mattbesedick <mattbezy@gmail.com>
2020-07-23 14:00:22 -0500

    gh pages

--- src/Cake.js
+++ src/Cake.js
@@ -1,23 +1,17 @@
-import React, { useEffect, useState, useMemo, useRef } from 'react'
+import React, { useRef } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { useSphere, useBox, useCylinder, useConvexPolyhedron, usePointToPointConstraint } from 'use-cannon'
 import { useLoader, useFrame } from 'react-three-fiber'
 
-const Cake = (props) => {
+const Cake = () => {
+    //loading model into scene
     const model = useRef()
     const gltf = useLoader(GLTFLoader, '/scene.gltf')
-    // const [ref] = useBox(() => ({ mass: 3, position: [0, 5, -2], rotation: [0.2, 0, 0] }))
     useFrame(() => {
-
         model.current.rotation.y -= .005
-
     })
+
     return (
         gltf ? <primitive ref={model} object={gltf.scene} position={[0, 0, -2]} scale={[.02, .02, .02]} /> : null
-        // <mesh position={[0, 0, -10]} rotation={[5, 0, 0]}>
-        //     <bufferGeometry attach="geometry"  {...gltf} />
-        //     <meshPhysicalMaterial attach="material" {...gltf} name="Material" />
-        // </mesh>
     )
 }
 
--- src/Confetti.js
+++ src/Confetti.js
@@ -1,18 +1,13 @@
-import React, { useRef, useMemo } from 'react'
-import { useThree, useFrame } from 'react-three-fiber'
-import * as THREE from 'three'
+import React from 'react'
 import { useBox } from 'use-cannon'
 
 const Confetti = ({ count }) => {
     const [ref, api] = useBox(() => ({ mass: 1, position: [Math.random() - 0.5, 5, Math.random() - 0.5] }))
 
-    // useFrame(() => {
-    // })
+
 
     return (
         <>
-            {/* <pointLight distance={40} intensity={8} color="lightblue" /> */}
-
             <instancedMesh ref={ref} position={[0, 0, -2]} args={[null, null, count]}>
                 <boxBufferGeometry attach="geometry" args={[0.2, 0.01, 0.1]} />
                 <meshBasicMaterial attach="material" color="red" />
